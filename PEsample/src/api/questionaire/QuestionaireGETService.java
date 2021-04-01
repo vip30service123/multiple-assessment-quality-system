b8f9fc4c0e9a7fb34b5ab1b7985f6bbb14491106
ngoquocthai0311 <ngoquocthai0311@gmail.com>
2021-04-01 23:35:56 +0700

    backend-GetClassInfo-01/04/2021

---
+++ PEsample/src/api/questionaire/QuestionaireGETService.java
@@ -0,0 +1,51 @@
+package api.questionaire;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.vgu.sqm.questionnaire.Configuration;
+
+@Path("/questionaire")
+public class QuestionaireGETService {
+	
+	@Path("/info/{classID}")
+	@GET
+	public Response getClassInfo(@PathParam("classID") String id) throws SQLException, NamingException{
+		Connection db = Configuration.getAcademiaConnection();
+		try {
+			JsonArrayBuilder classInfoArrayBuilder = Json.createArrayBuilder();
+			
+			PreparedStatement st = db.prepareStatement(
+					"call GetClassesInfo(?)");
+			st.setString(0, id);
+			
+			ResultSet rs = st.executeQuery();
+			System.out.println(rs);
+			
+			while (rs.next()) {
+				JsonObject entry = Json.createObjectBuilder()
+						.add("academic_name", rs.getString(0))
+						.add("semester_name", rs.getString(1))
+						.add("faculty_name", rs.getString(2))
+						.add("program_name", rs.getString(3))
+						.add("module_name", rs.getString(4)).build();
+				classInfoArrayBuilder.add(entry);
+			}
+			return Response.ok().entity(classInfoArrayBuilder.build().toString()).build();
+		}
+		finally {
+			db.close();
+		}
+	}
+}
