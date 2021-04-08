802f7654df619bc750b3ee5f6dd4d1f32e25fc22
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-08 19:50:20 +0700

    Update GetQuestion9Graph.sql

--- Database/Graph/GetQuestion9Graph.sql
+++ Database/Graph/GetQuestion9Graph.sql
@@ -39,14 +39,14 @@ BEGIN
 	SELECT 
 		
         COUNT(*) AS N,
-        ROUND((COUNT(*)/Class.size)*100,2) AS RESP_RATE,
-        ROUND(AVG(Q5),2) AS MEAN,
-        ROUND(stddev_samp(Q5),2) AS SD,
-        ROUND((SUM(case when Q9 = '1' then 1 else 0 end)/Class.size)*100,2) AS PERCENTAGE_OF_1,
-        ROUND((SUM(case when Q9 = '2' then 1 else 0 end)/Class.size)*100,2) AS PERCENTAGE_OF_2,
-        ROUND((SUM(case when Q9 = '3' then 1 else 0 end)/Class.size)*100,2) AS PERCENTAGE_OF_3,
-        ROUND((SUM(case when Q9 = '4' then 1 else 0 end)/Class.size)*100,2) AS PERCENTAGE_OF_4,
-        ROUND((SUM(case when Q9 = '5' then 1 else 0 end)/Class.size)*100,2) AS PERCENTAGE_OF_5
+        ROUND((COUNT(*)/SUM(Class.size))*100,2) AS RESP_RATE,
+        ROUND(AVG(Q9),2) AS MEAN,
+        ROUND(stddev_samp(Q9),2) AS SD,
+        ROUND((SUM(case when Q9 = '1' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_1,
+        ROUND((SUM(case when Q9 = '2' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_2,
+        ROUND((SUM(case when Q9 = '3' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_3,
+        ROUND((SUM(case when Q9 = '4' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_4,
+        ROUND((SUM(case when Q9 = '5' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_5
         
 	FROM Questionnaire 
 		NATURAL JOIN Aca_Faculty
