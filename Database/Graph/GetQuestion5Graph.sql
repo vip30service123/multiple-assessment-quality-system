e146d56bd5d6d4ce2efa8d2919ff6faeaeb4b244
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-09 11:42:45 +0700

    Fix rate of 5

--- Database/Graph/GetQuestion5Graph.sql
+++ Database/Graph/GetQuestion5Graph.sql
@@ -42,11 +42,11 @@ BEGIN
         ROUND((COUNT(*)/SUM(Class.size))*100,2) AS RESP_RATE,
         ROUND(AVG(Q5),2) AS MEAN,
         ROUND(stddev_samp(Q5),2) AS SD,
-        ROUND((SUM(case when Q5 = '1' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_1,
-        ROUND((SUM(case when Q5 = '2' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_2,
-        ROUND((SUM(case when Q5 = '3' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_3,
-        ROUND((SUM(case when Q5 = '4' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_4,
-        ROUND((SUM(case when Q5 = '5' then 1 else 0 end)/SUM(Class.size))*100,2) AS PERCENTAGE_OF_5
+        ROUND((SUM(case when Q5 = '1' then 1 else 0 end)/COUNT(*))*100,2) AS PERCENTAGE_OF_1,
+        ROUND((SUM(case when Q5 = '2' then 1 else 0 end)/COUNT(*))*100,2) AS PERCENTAGE_OF_2,
+        ROUND((SUM(case when Q5 = '3' then 1 else 0 end)/COUNT(*))*100,2) AS PERCENTAGE_OF_3,
+        ROUND((SUM(case when Q5 = '4' then 1 else 0 end)/COUNT(*))*100,2) AS PERCENTAGE_OF_4,
+        ROUND((SUM(case when Q5 = '5' then 1 else 0 end)/COUNT(*))*100,2) AS PERCENTAGE_OF_5
         
 	FROM Questionnaire 
 		NATURAL JOIN Aca_Faculty
