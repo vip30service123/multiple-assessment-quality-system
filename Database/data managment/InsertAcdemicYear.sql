3e3be0384025abde5626ff18c0dfeb439fb6502b
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-15 18:57:47 +0700

    Delete InsertAcdemicYear.sql

--- Database/data managment/InsertAcdemicYear.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertAcdemicYear`(AYCode VARCHAR(6), AYName VARCHAR(50))
-BEGIN
-    INSERT INTO academic_year (AYCode, AYName) VALUES (AYCode, AYName);
-END
