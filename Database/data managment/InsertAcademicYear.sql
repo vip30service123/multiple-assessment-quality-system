9efec64ce9e9f1630e41d51d9bda79fee96f0fd7
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-04-15 18:54:53 +0700

    Update InsertAcademicYear.sql

--- Database/data managment/InsertAcademicYear.sql
+++ Database/data managment/InsertAcademicYear.sql
@@ -4,8 +4,9 @@ BEGIN
     SET flag=0;
 	SET flag =(SELECT SUM(CheckAcademicYear(AYCode,AYName)) FROM academic_year);
     IF flag=0 THEN
+		SELECT 'Success';
 		INSERT INTO academic_year (AYCode, AYName) VALUES (AYCode, AYName);
 	ELSE 
-		select 'Duplicate';
+		SELECT 'Duplicate';
     END IF;
 END
