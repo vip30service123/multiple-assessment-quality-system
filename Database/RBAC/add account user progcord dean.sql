5bf3ce69f6460b09d2666f9675f0f283705b9f36
LamXuanBach <80671315+LamXuanBach@users.noreply.github.com>
2021-04-16 15:48:21 +0700

    Add files via upload

---
+++ Database/RBAC/add account user progcord dean.sql
@@ -0,0 +1,119 @@
+INSERT INTO account(Login,Pass) VALUES    ('VanHungT1','cokxIKCX');
+INSERT INTO account(Login,Pass) VALUES    ('LyraiL1','oqPNmc@@');
+INSERT INTO account(Login,Pass) VALUES    ('LinaM1','^mqOBJA$');
+INSERT INTO account(Login,Pass) VALUES    ('JimR','lecturer');
+INSERT INTO account(Login,Pass) VALUES    ('ElizabethE','#NHgYNyx');
+INSERT INTO account(Login,Pass) VALUES    ('SarahK','KApWDMmH');
+INSERT INTO account(Login,Pass) VALUES    ('SylvannasW','VrmfJbl*');
+INSERT INTO account(Login,Pass) VALUES    ('TimJ','NiVJFvxq');
+INSERT INTO account(Login,Pass) VALUES    ('JonesM','^nF!thEI');
+INSERT INTO account(Login,Pass) VALUES    ('CaoDuyH1','Xs!*qpll');
+INSERT INTO account(Login,Pass) VALUES    ('VincentL1','GiuZWMkt');
+INSERT INTO account(Login,Pass) VALUES    ('AnathanP1','#OIHOmyS');
+INSERT INTO account(Login,Pass) VALUES    ('RichardB1','aFDGPrVQ');
+INSERT INTO account(Login,Pass) VALUES    ('MinhThaiN1','ciTfrZe&');
+INSERT INTO account(Login,Pass) VALUES    ('SioneA1','PKyD&r!r');
+INSERT INTO account(Login,Pass) VALUES    ('ArtoriaP','KX$PxAfo');
+INSERT INTO account(Login,Pass) VALUES    ('TruongDuongL','VgTpNYmm');
+INSERT INTO account(Login,Pass) VALUES    ('AlexeiS1','rpesjRrc');
+INSERT INTO account(Login,Pass) VALUES    ('NolvaC1','WPKC@TLp');
+INSERT INTO account(Login,Pass) VALUES    ('TomJ','$BMhYGWy');
+INSERT INTO account(Login,Pass) VALUES    ('CaoDuyH2','l!KFfomQ');
+INSERT INTO account(Login,Pass) VALUES    ('VincentL2','HfiDSMRF');
+INSERT INTO account(Login,Pass) VALUES    ('AnathanP2','ELubwGEy');
+INSERT INTO account(Login,Pass) VALUES    ('RichardB2','bp!WXPfR');
+INSERT INTO account(Login,Pass) VALUES    ('MinhThaiN2','BwM$DdiK');
+INSERT INTO account(Login,Pass) VALUES    ('SioneA2','s*!t*oDR');
+INSERT INTO account(Login,Pass) VALUES    ('VanHungT2','idB%qCul');
+INSERT INTO account(Login,Pass) VALUES    ('LyraiL2','jxTVrv@k');
+INSERT INTO account(Login,Pass) VALUES    ('LinaM2','DKA&O^iC');
+INSERT INTO account(Login,Pass) VALUES    ('AlexeiS2','lZeJIX$$');
+INSERT INTO account(Login,Pass) VALUES    ('NolvaC2','ek&rws^o');
+INSERT INTO account(Login,Pass) VALUES    ('PCHistory','program0');
+INSERT INTO account(Login,Pass) VALUES    ('PCGeography','gVPwOBoc');
+INSERT INTO account(Login,Pass) VALUES    ('PCFinancial','bpM$xnLN');
+INSERT INTO account(Login,Pass) VALUES    ('PCBusiness','OzbwfvT#');
+INSERT INTO account(Login,Pass) VALUES    ('PCAutomation','HAjfepaP');
+INSERT INTO account(Login,Pass) VALUES    ('PCMechanic','lGH*SXrX');
+INSERT INTO account(Login,Pass) VALUES    ('PCSoftware','tSQ&o$i&');
+INSERT INTO account(Login,Pass) VALUES    ('PCAI','ou$rVjyf');
+INSERT INTO account(Login,Pass) VALUES    ('PCDP','mRiTrjsX');
+INSERT INTO account(Login,Pass) VALUES    ('PCSculpture','D*mHXevV');
+INSERT INTO account(Login,Pass) VALUES    ('FSocial','faculty0');
+INSERT INTO account(Login,Pass) VALUES    ('FEconomic','HEjFbLbT');
+INSERT INTO account(Login,Pass) VALUES    ('FEngineering','c%onAMiX');
+INSERT INTO account(Login,Pass) VALUES    ('FIT','IB@$bGal');
+INSERT INTO account(Login,Pass) VALUES    ('FArt','olpfb!Hd');
+
+
+
+
+INSERT INTO user (Username, Login) VALUES    ('Tran Van Hung1','VanHungT1');
+INSERT INTO user (Username, Login) VALUES   ('Lyrai Luna1','LyraiL1');
+INSERT INTO user (Username, Login)  VALUES  ('Lina Mirana1','LinaM1');
+INSERT INTO user (Username, Login)  VALUES  ('Jim Raynor','JimR');
+INSERT INTO user (Username, Login)  VALUES  ('Elizabeth Ezalor','ElizabethE');
+INSERT INTO user (Username, Login)  VALUES  ('Sarah Kerrigan','SarahK');
+INSERT INTO user (Username, Login)  VALUES  ('Syvannas Windrunner','SyvannasW');
+INSERT INTO user (Username, Login)  VALUES  ('Tim Johnson','TimJ');
+INSERT INTO user (Username, Login)   VALUES ('Jones McDonald','JonesM');
+INSERT INTO user (Username, Login)  VALUES ('Tran Cao Duy1','CaoDuyT1');
+INSERT INTO user (Username, Login)  VALUES ('Vincent Law1','VincentL1');
+INSERT INTO user (Username, Login) VALUES ('Anathan Pham1','AnathanP1');
+INSERT INTO user (Username, Login) VALUES  ('Richard Bradley1','RichardB1');
+INSERT INTO user (Username, Login) VALUES  ('Ngo Minh Thai1','MinhThaiN1');
+INSERT INTO user (Username, Login)  VALUES  ('Sione Aldric1','SioneA1');
+INSERT INTO user (Username, Login)  VALUES  ('Artoria Pendragon','ArtoriaP');
+INSERT INTO user (Username, Login)  VALUES  ('Le Truong Duong','TruongDuongL');
+INSERT INTO user (Username, Login)  VALUES ('Alexei Stukov','AlexeiS1');
+INSERT INTO user (Username, Login)  VALUES  ('Nolva Carpenter','NolvaC1');
+INSERT INTO user (Username, Login) VALUES  ('Tom Jerry','TomJ');
+INSERT INTO user (Username, Login)  VALUES  ('Tran Cao Duy2','CaoDuyT2');
+ INSERT INTO user (Username, Login) VALUES  ('Vincent Law2','VincentL2');
+INSERT INTO user (Username, Login) VALUES   ('Anathan Pham2','AnathanP2');
+INSERT INTO user (Username, Login)  VALUES  ('Richard Bradley2','RichardB2');
+INSERT INTO user (Username, Login)  VALUES  ('Ngo Minh Thai2','MinhThaiN2');
+INSERT INTO user (Username, Login) VALUES   ('Sione Aldric2','SioneA2');
+INSERT INTO user (Username, Login) VALUES   ('Tran Van Hung2','VanHungT2');
+INSERT INTO user (Username, Login) VALUES   ('Lyrai Luna2','LyraiL2');
+INSERT INTO user (Username, Login)  VALUES  ('Lina Mirana2','LinaM2');
+INSERT INTO user (Username, Login) VALUES   ('Alexei Stukov2','AlexeiS2');
+ INSERT INTO user (Username, Login) VALUES  ('Nolva Carpenter2','NolvaC2');
+INSERT INTO user (Username, Login)    VALUES ('Josh Walkman','PCHistory');
+INSERT INTO user (Username, Login)  VALUES  ('John Runnman','PCGeography');
+INSERT INTO user (Username, Login)   VALUES ('Joe Headman','PCFinancial');
+INSERT INTO user (Username, Login)   VALUES ('Joel Oman','PCBusiness');
+INSERT INTO user (Username, Login)  VALUES  ('Tim Burton','PCAutomation');
+INSERT INTO user (Username, Login)   VALUES ('Jim Burrow','PCMechanic');
+ INSERT INTO user (Username, Login)  VALUES ('Mark Regson','PCSoftware');
+INSERT INTO user (Username, Login)   VALUES ('Jack Sparrow','PCAI');
+INSERT INTO user (Username, Login)   VALUES ('Miranda Alonso','PCDP');
+INSERT INTO user (Username, Login)   VALUES ('Angelia Carlos','PCSculpture');
+INSERT INTO user (Username, Login)  VALUES  ('Tarzan Jungle','FSocial');
+ INSERT INTO user (Username, Login) VALUES  ('Harry Apemann','FEconomic');
+ INSERT INTO user (Username, Login) VALUES  ('Alex Mercer','FEngineering');
+INSERT INTO user (Username, Login) VALUES  ('Amelia Watson','FIT');
+INSERT INTO user (Username, Login) VALUES   ('Gawr Gura','FArt');
+
+
+
+
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES    ('2020-09-15','2021-07-15','PCHistory','P10001');
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES     ('2020-09-15','2021-07-15','PCGeography','P10002');
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES     ('2020-09-15','2021-07-15','PCFinancial','P20001');
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES     ('2020-09-15','2021-07-15','PCBusiness','P20002');
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES     ('2020-09-15','2021-07-15','PCAutomation','P30001');
+INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES     ('2020-09-15','2021-07-15','PCMechanic','P30002');
+ INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES    ('2020-09-15','2021-07-15','PCSoftware','P40001');
+ INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES    ('2020-09-15','2021-07-15','PCAI','P40002');
+ INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES    ('2020-09-15','2021-07-15','PCDP','P50001');
+ INSERT INTO Prog_Cordinator (StartDate, EndDate,	Username, PCode) VALUES    ('2020-09-15','2021-07-15','PCSculpture','P50002');
+ 
+ 
+ 
+ 
+ INSERT INTO dean (StartDate, EndDate,	Username, FCode)VALUES    ('2020-09-15','2021-07-15','FSocial','F00001');
+INSERT INTO dean (StartDate, EndDate,	Username, FCode)VALUES    ('2020-09-15','2021-07-15','FEconomic','F00002');
+INSERT INTO dean (StartDate, EndDate,	Username, FCode)VALUES    ('2020-09-15','2021-07-15','FEngineering','F00003');
+INSERT INTO dean (StartDate, EndDate,	Username, FCode)VALUES    ('2020-09-15','2021-07-15','FIT','F00004');
+INSERT INTO dean (StartDate, EndDate,	Username, FCode)VALUES    ('2020-09-15','2021-07-15','FArt','F00005');
