d273a9f9919c2edc1f71d550fcd0d55eaf0fe0a0
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-16 10:18:08 +0700

    Update getInfoYearFacPro.sql

--- Database/data managment/getInfoYearFacPro.sql
+++ Database/data managment/getInfoYearFacPro.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getInfoYearFacPro`()
 BEGIN
-SELECT PFCode
-FROM aca_fac_pro
+SELECT PFCode,AYName, FName,PName
+FROM aca_fac_pro NATURAL JOIN faculty NATURAL JOIN Aca_Faculty NATURAL JOIN academic_year NATURAL JOIN program
 ORDER BY PFCode;
 END
