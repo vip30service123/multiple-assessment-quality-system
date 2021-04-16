e516d0d62bfaaa828a311fd793dec0cacb98be2d
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-16 10:12:54 +0700

    Update getInfoYearFac.sql

--- Database/data managment/getInfoYearFac.sql
+++ Database/data managment/getInfoYearFac.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getInfoYearFac`()
 BEGIN
 SELECT AFCode,AYName,FName
-FROM Aca_Faculty
+FROM Aca_Faculty NATURAL JOIN academic_year NATURAL JOIN faculty
 ORDER BY AFCode;
 END
