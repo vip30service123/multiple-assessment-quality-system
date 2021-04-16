c439da6c78ceaafa6a4ca7f3ab63bc0ec452c6f9
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-16 13:22:02 +0700

    Update DeleteInfoYearFacPro.sql

--- Database/data managment/DeleteInfoYearFacPro.sql
+++ Database/data managment/DeleteInfoYearFacPro.sql
@@ -1,5 +1,5 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteInfoYearFacPro`(pro_fac_code VARCHAR(6))
 BEGIN
 	DELETE FROM aca_fac_pro 
-	WHERE AFCode LIKE (PFCode LIKE pro_fac_code);
+	WHERE PFCode LIKE pro_fac_code;
 END
