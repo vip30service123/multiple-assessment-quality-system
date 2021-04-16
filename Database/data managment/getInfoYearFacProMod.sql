d034cac133950a9b687fc1c2a20667588f8c02f2
TomtheCodeBot <65719931+TomtheCodeBot@users.noreply.github.com>
2021-04-16 10:18:10 +0700

    Update getInfoYearFacProMod.sql

--- Database/data managment/getInfoYearFacProMod.sql
+++ Database/data managment/getInfoYearFacProMod.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getInfoYearFacProMod`()
 BEGIN
-SELECT PFCode, MCode
-FROM aca_pro_mod
+SELECT PFCode, MCode,AYName, FName,PName,MName
+FROM aca_pro_mod NATURAL JOIN module NATURAL JOIN aca_fac_pro NATURAL JOIN faculty NATURAL JOIN Aca_Faculty NATURAL JOIN academic_year NATURAL JOIN program
 ORDER BY PFCode, MCode;
 END
